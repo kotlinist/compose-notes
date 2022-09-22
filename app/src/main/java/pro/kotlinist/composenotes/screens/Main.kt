25955cc2c759bd31c4ebe524ab806990c3908c79
Anatoly <a.jabrov753@gmail.com>
2022-09-22 19:18:19 +0300

    Navigation

---
+++ app/src/main/java/pro/kotlinist/composenotes/screens/Main.kt
@@ -0,0 +1,8 @@
+package pro.kotlinist.composenotes.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Main(navController: NavHostController ) {
+}
